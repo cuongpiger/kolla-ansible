7f4c60865da04c64019cf0c82399c7a0bac6243a
cuongpiger <cuongpigerr@gmail.com>
2023-06-05 11:29:08 +0700

    [feat] - only deploy for Keystone - testing for OSprofiler

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -6,28 +6,26 @@ set -o errexit
 # Enable unbuffered output for Ansible in Jenkins.
 export PYTHONUNBUFFERED=1
 
-
 function deploy {
-    RAW_INVENTORY=/etc/kolla/inventory
+  RAW_INVENTORY=/etc/kolla/inventory
 
-    source $KOLLA_ANSIBLE_VENV_PATH/bin/activate
+  source $KOLLA_ANSIBLE_VENV_PATH/bin/activate
 
-    #TODO(inc0): Post-deploy complains that /etc/kolla is not writable. Probably we need to include become there
-    sudo chmod -R 777 /etc/kolla
-    # generate self-signed certificates for the optional internal TLS tests
-    if [[ "$TLS_ENABLED" = "True" ]]; then
-        kolla-ansible -i ${RAW_INVENTORY} -vvv certificates > /tmp/logs/ansible/certificates
-    fi
-    # Actually do the deployment
-    kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks &> /tmp/logs/ansible/deploy-prechecks
-    kolla-ansible -i ${RAW_INVENTORY} -vvv pull &> /tmp/logs/ansible/pull
-    kolla-ansible -i ${RAW_INVENTORY} -vvv deploy &> /tmp/logs/ansible/deploy
-    kolla-ansible -i ${RAW_INVENTORY} -vvv post-deploy &> /tmp/logs/ansible/post-deploy
+  #TODO(inc0): Post-deploy complains that /etc/kolla is not writable. Probably we need to include become there
+  sudo chmod -R 777 /etc/kolla
+  # generate self-signed certificates for the optional internal TLS tests
+  if [[ "$TLS_ENABLED" = "True" ]]; then
+    kolla-ansible -i ${RAW_INVENTORY} -vvv certificates >/tmp/logs/ansible/certificates
+  fi
+  # Actually do the deployment
+  kolla-ansible -i ${RAW_INVENTORY} -vvv prechecks &>/tmp/logs/ansible/deploy-prechecks
+  kolla-ansible -i ${RAW_INVENTORY} -vvv pull &>/tmp/logs/ansible/pull
+  kolla-ansible -i ${RAW_INVENTORY} -vvv deploy &>/tmp/logs/ansible/deploy
+  kolla-ansible -i ${RAW_INVENTORY} -vvv post-deploy &>/tmp/logs/ansible/post-deploy
 
-    if [[ $HAS_UPGRADE == 'no' ]]; then
-        kolla-ansible -i ${RAW_INVENTORY} -vvv validate-config &> /tmp/logs/ansible/validate-config
-    fi
+  if [[ $HAS_UPGRADE == 'no' ]]; then
+    kolla-ansible -i ${RAW_INVENTORY} -vvv validate-config &>/tmp/logs/ansible/validate-config
+  fi
 }
-
 
 deploy
